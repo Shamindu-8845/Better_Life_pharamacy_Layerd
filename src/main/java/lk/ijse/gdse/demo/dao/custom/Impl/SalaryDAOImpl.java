ac15ecf7e560f4d82ce9b39360d803c3da6c615d
shamindu <kssds1212@gmail.com>
2025-02-05 15:16:47 +0530

    salarydaoimpl is with done sqlutil

--- src/main/java/lk/ijse/gdse/demo/dao/custom/Impl/SalaryDAOImpl.java
+++ src/main/java/lk/ijse/gdse/demo/dao/custom/Impl/SalaryDAOImpl.java
@@ -15,28 +15,6 @@ public class SalaryDAOImpl implements SalaryDAO {
 
     @Override
     public ObservableList<Salary> getAll() throws SQLException {
-       /* // Query to select all details from the Salary table
-        String query = "SELECT Salary_Id, Amount, Date FROM Salary";
-
-        // Execute the query using CrudUtil (Assumed utility class to execute SQL queries)
-        ResultSet resultSet = CrudUtil.execute(query);
-
-        // Create an ObservableList to hold SalaryDTO objects
-        ObservableList<Salary> salaryList = FXCollections.observableArrayList();
-
-        // Iterate through the result set and create SalaryDTO objects
-        while (resultSet.next()) {
-            Salary salaryDTO = new Salary(
-                    resultSet.getString("Salary_Id"),
-                    resultSet.getDouble("Amount"), // Amount is a decimal type
-                    resultSet.getDate("Date")           // Date is a date type
-            );
-            salaryList.add(salaryDTO);
-        }
-
-        // Return the list of SalaryDTO objects
-        return salaryList;*/
-
         ResultSet resultSet = SQLUtil.executeQuery("SELECT Salary_Id, Amount, Date FROM Salary");
         ObservableList<Salary> salaries = FXCollections.observableArrayList();
         while (resultSet.next()) {
@@ -53,12 +31,6 @@ public class SalaryDAOImpl implements SalaryDAO {
 
     @Override
     public boolean add(Salary salaryDTO) throws SQLException {
-        /*return CrudUtil.execute(
-                "INSERT INTO Salary (Salary_Id, Amount, Date) VALUES (?, ?, ?)",
-                salaryDTO.getSalary_Id(),
-                salaryDTO.getAmount(),
-                salaryDTO.getDate()
-        );*/
        return SQLUtil.execute("INSERT INTO Salary (Salary_Id, Amount, Date) VALUES (?, ?, ?)",
                 salaryDTO.getSalary_Id(),
                 salaryDTO.getAmount(),
@@ -67,13 +39,6 @@ public class SalaryDAOImpl implements SalaryDAO {
     }
     @Override
     public boolean update(Salary salaryDTO) throws SQLException {
-        /*return CrudUtil.execute(
-                "UPDATE Salary SET Amount=?, Date=? WHERE Salary_Id=?",
-                salaryDTO.getAmount(),
-                salaryDTO.getDate(),
-                salaryDTO.getSalary_Id()
-        );*/
-
        return SQLUtil.execute("UPDATE Salary SET Amount=?, Date=? WHERE Salary_Id=?",
                 salaryDTO.getAmount(),
                 salaryDTO.getDate(),
@@ -88,19 +53,14 @@ public class SalaryDAOImpl implements SalaryDAO {
             String subString = lastId.substring(1);
             int I = Integer.parseInt(subString);
             int newIndex = I + 1;
-            return String.format("S%04d", newIndex);  // Adjust the format if your `Salary_Id` has different length requirements
+            return String.format("S%04d", newIndex);
         }
 
         return "S0001";
     }
     @Override
     public boolean delete(String Id) throws SQLException {
-        /*return CrudUtil.execute(
-                "DELETE FROM salary WHERE Salary_Id=?",
-                Id
-        );*/
-
-       return SQLUtil.execute("DELETE FROM salary WHERE Salary_Id=?",Id);
+        return SQLUtil.execute("DELETE FROM salary WHERE Salary_Id=?",Id);
     }
 
     public boolean updateEmployee(EmployeeDTO employeeDTO) throws SQLException {
